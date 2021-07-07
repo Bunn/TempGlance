59936ac4f7533ae98672241263d461b33523d6c1
Fernando Bunn <bunn@idevzilla.com>
2021-07-07 10:05:31 +0100

    initial commit

---
+++ TempGlance/Shared/TempGlanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempGlanceApp.swift
+//  Shared
+//
+//  Created by Fernando Bunn on 07/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct TempGlanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
